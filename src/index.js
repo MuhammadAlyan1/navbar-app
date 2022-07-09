5a9c1797fd049fcc2d69c6913515922480d25681
Muhammad Alyan <alyan0332@gmail.com>
2022-07-09 12:35:07 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import NavBar from "./NavBar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
